954a2a43b35103cbca341034501a50afa9e80103
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-06-05 20:07:19 +0300

    refactor modalActions

---
+++ src/redux/actions/modalActions.ts
@@ -0,0 +1,17 @@
+import { SET_MODAL_OPEN, SET_ALERT_SHOWN } from "./actionTypes";
+
+type SetModalActionType = { type: typeof SET_MODAL_OPEN; isOpen: boolean };
+export const setModal = (isOpen: boolean): SetModalActionType => {
+  return {
+    type: SET_MODAL_OPEN,
+    isOpen,
+  };
+};
+
+type SetAlertActionType = { type: typeof SET_ALERT_SHOWN; isShown: boolean };
+export const setAlert = (isShown: boolean): SetAlertActionType => {
+  return {
+    type: SET_ALERT_SHOWN,
+    isShown,
+  };
+};
