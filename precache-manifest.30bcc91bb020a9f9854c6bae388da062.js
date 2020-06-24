8be19857af111232a4d5eacc203c97acee766081
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-06-24 21:33:15 +0300

    Updates

---
+++ precache-manifest.30bcc91bb020a9f9854c6bae388da062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718b56dd641212e96ff6d4f905621fe5",
+    "url": "/test-from-mtg-typescript/index.html"
+  },
+  {
+    "revision": "4ef29eee84bf7e09a575",
+    "url": "/test-from-mtg-typescript/static/css/main.985f83d8.chunk.css"
+  },
+  {
+    "revision": "83c7ba5093683d7f9b40",
+    "url": "/test-from-mtg-typescript/static/js/2.dd87ccaa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-from-mtg-typescript/static/js/2.dd87ccaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ef29eee84bf7e09a575",
+    "url": "/test-from-mtg-typescript/static/js/main.4e1ece5a.chunk.js"
+  },
+  {
+    "revision": "2fdd62ea23bb4b6339c7",
+    "url": "/test-from-mtg-typescript/static/js/runtime-main.c25732f2.js"
+  }
+]);
